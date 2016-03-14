c21a7ecfa447c3f0744c7fab807fd5573cbe7f8f
Christine Tacha and Oskar Radon <oskarradonkimball@gmail.com>
2016-03-14 10:01:49 -0700

    Fix file path in UI logic require statement

--- build/js/app.js
+++ build/js/app.js
@@ -1,10 +1,29 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 
+// business logic
+
+// entry constructor
+
+function Entry (title, text) {
+  this.title = title;
+  this.text = text;
+}
+
+// method of counting characters in entry
+
+Entry.prototype.countCharacters = function(text) {
+  return text.length();
+}
+
+exports.Entry = Entry;
+
+},{}],2:[function(require,module,exports){
+
 // interface logic
 
 //requires
 
-var Entry = require('./journal.js').Entry;
+var Entry = require('./../js/journal.js').Entry;
 
 // get values from input fields on submit
 
@@ -15,19 +34,14 @@ $(function() {
     var title = $('.title').val();
     var text = $('text').val();
     var newEntry = new Entry(title, text);
-
+    // run char counting method
     var EntryLength = newEntry.countCharacters(text);
-
+    // append entry to #entries div
     $('#entries').append('<article><h3>' + title + '<h3><p>' + text + '</p><p>' + EntryLength + ' letters</p></article>');
 
   });
 });
 
-// run char counting method
-
-// append entry to #entries div
-
-},{"./journal.js":2}],2:[function(require,module,exports){
 
 // business logic
 
@@ -46,4 +60,4 @@ Entry.prototype.countCharacters = function(text) {
 
 exports.Entry = Entry;
 
-},{}]},{},[1]);
+},{"./../js/journal.js":1}]},{},[2]);
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -21,3 +21,21 @@ $(function() {
 
   });
 });
+
+
+// business logic
+
+// entry constructor
+
+function Entry (title, text) {
+  this.title = title;
+  this.text = text;
+}
+
+// method of counting characters in entry
+
+Entry.prototype.countCharacters = function(text) {
+  return text.length();
+}
+
+exports.Entry = Entry;
