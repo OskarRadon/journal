eb61441a36e0193211a398b0b93be1805e37acf0
Christine Tacha and Oskar Radon <tacha.christine@gmail.com>
2016-03-14 09:31:34 -0700

    Browserify app.js file

---
+++ build/js/app.js
@@ -0,0 +1,49 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+
+// interface logic
+
+//requires
+
+var Entry = require('./journal.js').Entry;
+
+// get values from input fields on submit
+
+$(function() {
+  event.preventDefault();
+
+  $('.journalInput').submit(function(event) {
+    var title = $('.title').val();
+    var text = $('text').val();
+    var newEntry = new Entry(title, text);
+
+    var EntryLength = newEntry.countCharacters(text);
+
+    $('#entries').append('<article><h3>' + title + '<h3><p>' + text + '</p><p>' + EntryLength + ' letters</p></article>');
+
+  });
+});
+
+// run char counting method
+
+// append entry to #entries div
+
+},{"./journal.js":2}],2:[function(require,module,exports){
+
+// business logic
+
+// entry constructor
+
+function Entry (title, text) {
+  this.title = title;
+  this.text = text;
+}
+
+// method of counting characters in entry
+
+Entry.prototype.countCharacters = function(text) {
+  return text.length();
+}
+
+exports.Entry = Entry;
+
+},{}]},{},[1]);
