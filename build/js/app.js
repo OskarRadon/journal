79f9c21999785f6da5c41c6ca1e6d861d19544af
Christine Tacha and Oskar Radon <tacha.christine@gmail.com>
2016-03-14 11:34:47 -0700

    Make jQuery more specific

--- build/js/app.js
+++ build/js/app.js
@@ -29,14 +29,14 @@ var Entry = require('./../js/journal.js').Entry;
 
 $(function() {
 
-  $('.journalInput').submit(function(event) {
-    var title = $('.title').val();
-    var text = $('.text').val();
+  $('form.journalInput').submit(function(event) {
+    var title = $('input.title').val();
+    var text = $('textarea.text').val();
     var newEntry = new Entry(title, text);
     // run char counting method
     var EntryLength = newEntry.countCharacters();
     // append entry to #entries div
-    $('#entries').prepend('<article><h3>' + title + '<h3><p>' + text + '</p><p>' + EntryLength + ' characters</p></article>');
+    $('div#entries').prepend('<article><h3>' + title + '<h3><p>' + text + '</p><p>' + EntryLength + ' characters</p></article>');
 
     event.preventDefault();
   });
